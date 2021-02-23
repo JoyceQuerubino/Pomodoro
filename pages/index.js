798e99053f42e5dfa560a4fb1c8997277a0c1806
JoyceQuerubino <joycequerubino5@gmail.com>
2021-02-23 12:37:17 -0300

    Finalizando migraçao

--- pages/index.js
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-  return (
-    <h1>OIIII</h1>
-  )
-}
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { ExperienceBar } from "../components/ExperienceBar";
+
+export default function Home() {
+  return (
+    <div className="container">
+
+      <ExperienceBar />
+    </div>
+  )
+}
