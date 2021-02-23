0bf8654996106a548fcfe581f2e21f260168cbac
JoyceQuerubino <joycequerubino5@gmail.com>
2021-02-23 14:54:44 -0300

    Add imagem de level up

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -6,7 +6,10 @@ export function Profile(){
             <img src="https://avatars.githubusercontent.com/u/66806696?s=400&u=8d322799564bf4b0e72262ca04d5f01fca74bb16&v=4" alt="Joyce Querubino"/>
             <div>
                 <strong>Joyce Querubino</strong>
-                <p>Level 1</p>
+                <p>
+                    <img src="icons/level.svg" alt="Level"/>
+                    Level 1
+                </p>
             </div>
         </div>
     )
