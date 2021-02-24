179edd241fae68a19e4a8751985e0e3715214678
JoyceQuerubino <joycequerubino5@gmail.com>
2021-02-24 20:58:25 -0300

    Inserindo outros estilos ao botao

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -82,6 +82,7 @@ export function Countdown(){
                     className={styles.countdownButton}
                 >
                     Ciclo encerrado
+                    <img src="icons/check_circle.svg" alt="Check"/>
                 </button>
             ) : (
                 <>
