36105888a6f7b914b68f7870724b5f00bf04eb69
Joyce Querubino <joycequerubino5@gmail.com>
2021-02-24 21:47:28 -0300

    Adicionando estilização do componente ChallengeBox

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,15 +1,45 @@
 import styles from '../styles/components/ChallengeBox.module.css'
 
 export function ChallengeBox(){
+    const hasActiveChallenge = true;
+
     return(
         <div className={styles.challengeBoxContainer}>
-            <div className={styles.challengeNotActive}>
-                <strong>Finalize um ciclo para receber um desafio</strong>
-                <p>
-                    <img src="icons/level-up.svg" alt="Level Up"/>
-                    Avance de level completando desafios
-                </p>
-            </div>
+           { hasActiveChallenge ? (
+               <div className={styles.challengeActive}>
+                   <header>Ganhe 400 xp</header>
+
+                   <main>
+                       <img src="icons/body.svg" alt="concluído"/>
+                       <strong>Novo desafio</strong>
+                       <p>Levante e faça uma caminhada de 3 minutos.</p>
+                   </main>
+
+                    <footer>
+                        <button 
+                          type="button"
+                          className={styles.challengeFaildButton}
+                        >
+                            Falhei
+                        </button>
+                        <button 
+                          type="button"
+                          className={styles.challengeSuccededButton}
+                        >
+                            Completei
+                        </button>
+                    </footer>
+
+               </div>
+           ) : (
+                <div className={styles.challengeNotActive}>
+                    <strong>Finalize um ciclo para receber um desafio</strong>
+                    <p>
+                        <img src="icons/level-up.svg" alt="Level Up"/>
+                        Avance de level completando desafios
+                    </p>
+                </div>
+           ) }
         </div>
     )
 }
