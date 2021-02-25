8c08e6cbc8f9f094b047b0c593714357c283119b
JoyceQuerubino <joycequerubino5@gmail.com>
2021-02-25 12:09:53 -0300

    Finalizando contexto

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,10 +1,13 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Countdown.module.css'
 
 //Criar uma vaeriável para não deixar o tempo passar
 let countdownTimeout: NodeJS.Timeout;
 
 export function Countdown(){
+
+    const { startNewChallenge } = useContext(ChallengesContext)
 
     //Adicionar o tempo inicial em segundos (25min * 60s)
     const [time, setTime] = useState(0.1 * 60)
@@ -58,6 +61,7 @@ export function Countdown(){
         else if(isActive && time === 0){
             setHasFinished(true);
             setIsActive(false);
+            startNewChallenge();
         }
     }, [isActive, time])
 
--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,17 +1,40 @@
-import { createContext, useState } from 'react'; 
+import { createContext, useState, ReactNode } from 'react'; 
+interface ChallengesContextData{
+    level: number;
+    currentExperience: number;
+    challengesCompleted: number; 
+    levelUp: () => void; 
+    startNewChallenge: () => void;
+}
+interface ChallengesProviderProps{
+    children: ReactNode;
+}
 
-export const ChallengesContext = createContext({}); 
+export const ChallengesContext = createContext({} as ChallengesContextData); 
 
-export function ChallengesProvider({ children }){
+export function ChallengesProvider({ children }: ChallengesProviderProps){
 
     const [level, setlevel] = useState(1)
+    const [currentExperience, setCurrentExperience] = useState(0)
+    const [challengesCompleted, setChallengesCompleted] = useState(0)
 
     function levelUp(){
-      setlevel(level + 1)
-    }  
+      setlevel(level + 1); 
+    } 
+
+    function startNewChallenge(){
+        console.log('New challenge')
+    }
 
     return(
-        <ChallengesContext.Provider value={{ level, levelUp }}>
+        <ChallengesContext.Provider 
+        value={{ 
+            level, 
+            currentExperience, 
+            challengesCompleted, 
+            levelUp, 
+            startNewChallenge,
+        }}>
             {children}
         </ChallengesContext.Provider>
     )
