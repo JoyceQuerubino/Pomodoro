0a1d42ee7733d856604de5a8549a76371df17f86
JoyceQuerubino <joycequerubino5@gmail.com>
2021-03-01 11:07:45 -0300

    Finalizando cookies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,52 +1,66 @@
+import Head from 'next/head'
+import { GetServerSideProps } from 'next'; 
+
 import { CompleteChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 
-import Head from 'next/head'
 
 import styles from '../styles/Pages/Home.module.css';
 import { ChallengeBox } from "../components/ChallengeBox";
 import { CountdownProvider } from "../contexts/CountdownContext";
+import { ChallengesProvider } from '../contexts/ChallengesContext';
 
-export default function Home(props) {
+interface HomeProps{
+  level: number; 
+  currentExperience: number; 
+  challengesCompleted: number; 
+}
+
+export default function Home(props:HomeProps) {
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Inicio | Pomodoro.it</title>
-      </Head>
-      
-      <ExperienceBar />
+    <ChallengesProvider 
+      level={props.level} 
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
+      <div className={styles.container}>
+        <Head>
+          <title>Inicio | Pomodoro.it</title>
+        </Head>
+        
+        <ExperienceBar />
 
-      <CountdownProvider>
-        <section>
-         <div>
-            <Profile />
-            <CompleteChallenges />
-            <Countdown />
-          </div>
+        <CountdownProvider>
+          <section>
           <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountdownProvider>
-      
-    </div>
+              <Profile />
+              <CompleteChallenges />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+        
+      </div>
+    </ChallengesProvider>
   )
 }
 
 //Para manipular os dados que serão passados da camada front-end para a camada do Next.js
-export const getServerSideProps = async () => {
-  //chamando a api
-  const user = {
-    level: 1,
-    currentExperience: 50,
-    challengesCompleted: 2,   
-
-  }
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
   
+  const {level, currentExperience, challengesCompleted} = ctx.req.cookies;
+
   return {
-    props: user
+    props: {
+      level: Number(level), 
+      currentExperience: Number(currentExperience), 
+      challengesCompleted: Number(challengesCompleted)
+    }
   }
 }
